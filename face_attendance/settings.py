e729a9655b6f2e48e7e9ca9d9edc64d9b4421ce3
Lokesh <lokesh.sindhuluri@gmail.com>
2023-03-31 10:11:58 +0530

    updated settings secret

--- face_attendance/settings.py
+++ face_attendance/settings.py
@@ -20,15 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('/etc/secret_key.txt') as f:
+with open('secret.txt') as f:
     SECRET_KEY = f.read().strip()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+]
 
-
+SESSION_COOKIE_SECURE = True
 # Application definition
+CSRF_COOKIE_SECURE = True
 
 INSTALLED_APPS = [
     "django.contrib.admin",
