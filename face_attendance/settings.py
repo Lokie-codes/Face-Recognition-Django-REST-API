cff835e26b5ccda9183eeb3297638171a89bceb3
Lokesh S <79404752+Lokie-codes@users.noreply.github.com>
2023-04-14 18:49:38 +0000

    Final deployment change

--- face_attendance/settings.py
+++ face_attendance/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -154,6 +153,3 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-STATICFILES_DIRS = os.path.join(BASE_DIR, "staticfiles_build"),
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
