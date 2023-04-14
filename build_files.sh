7b3c64152a7457bfd1482d32016070887982bce9
Lokesh <lokesh.sindhuluri@gmail.com>
2023-04-14 21:58:23 +0530

    changing python3.11 to python 3

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 
 echo "BUILD START"
-python3.11 -m venv venv
+python3 -m venv venv
 source venv/bin/activate
 python3.11 -m pip install -r requirements.txt
 python3.11 manage.py collectstatic --noinput --clear
