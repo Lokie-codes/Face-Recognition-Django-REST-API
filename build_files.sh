795a63ff192157de284afb8af088c62ecfb20fc1
Lokesh <lokesh.sindhuluri@gmail.com>
2023-04-14 22:09:03 +0530

    removing venv

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,5 @@
 
 echo "BUILD START"
-python3 -m venv venv
-source venv/bin/activate
 python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --noinput --clear
 python3 manage.py makemigrations
